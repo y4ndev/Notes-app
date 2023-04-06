f3aeb7f7bb71f8abac2c5545d1099a7a5fa7565a
Yan Golovchenko <y4nweb@gmail.com>
2023-04-06 11:21:06 +0300

    pus

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet, Text, useColorScheme, View} from 'react-native';
+
+const Section = () => {
+  return (
+    <View>
+      <Text>Hello world</Text>
+    </View>
+  );
+};
+
+const App = () => {
+  return <Section />;
+};
+
+export default App;
